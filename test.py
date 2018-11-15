47a0a786b901498527575eb2478c84fb94ed3687
kasypArchit <archit.kashyap@iiitb.org>
2018-11-15 17:58:06 +0530

    CSV file added

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import user_data 
 
-# df = user_data.load_user_data()  
-df = user_data.user_rating_change(handle='Mindjolt')
+df = user_data.load_user_data()  
+# df = user_data.user_rating_change(handle='Mindjolt')
 # if df==3:
 #     print("Error while loading data")
 # else:
 #     print(df.head())
 print(df.head())
+df.to_csv('kashyap.csv')
